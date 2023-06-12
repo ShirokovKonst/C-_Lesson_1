1765a81554c807b2767b1ddcdd2977fd8d2fba56
Kostya <ashirokovkonstantin@gmail.com>
2023-06-12 21:31:39 +0500

    Creatre homework 3

---
+++ C#_lesson1_HomeWork3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C__lesson1_HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int a = int.Parse(Console.ReadLine()!);
+            if(a % 2 == 0)
+            Console.WriteLine("Число четное");
+            else
+            Console.WriteLine("Число нечетное");
+        }
+    }
+}
