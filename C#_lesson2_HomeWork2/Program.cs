7484182658593a5f3f560d4b350dd6dae920d370
Kostya <ashirokovkonstantin@gmail.com>
2023-06-14 21:10:06 +0500

    Create lesson 2 homework 2

---
+++ C#_lesson2_HomeWork2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C__lesson2_HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            if (num/10>1)
+            {
+                int result = num;
+                while (result>999)
+                {
+                    result = result/10;
+                }
+                result = result % 10;
+                Console.WriteLine("Третья цифра вашего числа: " + result);
+            }
+            else
+                Console.WriteLine("Третьей цифры в числе нет");
+        }
+    }
+}
