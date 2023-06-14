8a9e0a6f65fdebe83044c497b19d6e59d2e6bd0f
Kostya <ashirokovkonstantin@gmail.com>
2023-06-14 21:16:16 +0500

    Create lesson 2 homework 3

---
+++ C#_lesson2_HomeWork3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C__lesson2_HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цифру от 1 до 7");
+            int num = int.Parse(Console.ReadLine());
+            while ((num<1)^(num>7))
+            {
+                Console.WriteLine("Вы ввели цифру меньше 1 или больше 7!");
+                Console.WriteLine("Введите цифру от 1 до 7");
+                num = int.Parse(Console.ReadLine());
+            }
+            if (num >= 6)
+            Console.WriteLine("Да, это выходной");
+            else
+            Console.WriteLine("Нет, это рабочий день");
+        }
+    }
+}
