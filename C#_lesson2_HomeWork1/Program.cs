ad25ccaaf4d443ecf8f7d2842fac278a14d90b50
Kostya <ashirokovkonstantin@gmail.com>
2023-06-14 20:40:39 +0500

    Create lesson 2 homework 1

---
+++ C#_lesson2_HomeWork1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C__lesson2_HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число");
+            int num = int.Parse(Console.ReadLine());
+            while ((num/100<1)^(num/100>10))
+            {
+                Console.WriteLine("Вы ввели не трехзначное число!");
+                Console.WriteLine("Введите трехзначное число");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            int result = (num/10)%10;
+            Console.WriteLine("Вторая цифра числа: " + result);
+
+        }
+    }
+}
